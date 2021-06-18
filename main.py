61190bbbcd4137af89f2fd57134ed693d32218e4
Marius Stuparu <marius@mariusstuparu.com>
2021-06-18 21:46:58 +0300

    folder selection buttons wip

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ from functools import partial
 
 class ResolveAutomation:
     def __init__(self):
+        """
+        Initialize variables and load DaVinci scripting support
+        """
         global dvr
         self.errorMessages = []
-        self.resolve = None
         self.window = Tk()
-        self.selectedProject = ''
+        # Resolve specific
+        self.resolve = None
+        self.pm = None
+        self.selectedProject = None
 
         try:
             import DaVinciResolveScript as dvr
@@ -43,51 +48,84 @@ class ResolveAutomation:
                 self.resolve = dvr.scriptapp('Resolve')
             elif self.dvr:
                 self.resolve = self.dvr.scriptapp('Resolve')
+            self.pm = self.resolve.GetProjectManager()
             self.__startGUI()
 
-    def __startGUI(self):
+    def __startGUI(self) -> None:
+        """
+        Main GUI controller
+        """
         self.__basigGUIsetup()
+        self.__generateProjectSelectionButtons()
+
+        self.window.mainloop()
+
+    def __basigGUIsetup(self) -> None:
+        """
+        Generic window decoration and setup
+        """
+        self.window.title('DaVinci Resolve Automated Render')
+        self.window.geometry('800x500')
+        self.framePrjSelect = Frame(self.window, height=150, width=800)
+        self.framePrjSelect.pack(fill=X, pady=15)
+        self.frameFolderSelect = Frame(self.window, height=150, width=750, relief=SUNKEN)
+        self.frameFolderSelect.pack(fill=X, pady=15)
+
+    def __generateProjectSelectionButtons(self) -> None:
+        """
+        Load the project list from Resolve and generate selection buttons
+        """
         self.btnsProjects = []
 
-        projects = self.__getProjectsList()
-        framePrjSelect = Frame(self.window, height=150, width=800)
-        framePrjSelect.pack(fill=X)
-        framePrjButtons = Frame(framePrjSelect, height=50, width=800)
-        framePrjButtons.pack(side=BOTTOM)
+        projects = self.pm.GetProjectListInCurrentFolder()
+
+        framePrjButtons = Frame(self.framePrjSelect, height=50, width=800)
+        framePrjButtons.pack(side=BOTTOM, pady=5)
         if len(projects) <= 9:
-            prjLabel = Label(framePrjSelect, text='Select the project containing the sources:')
+            prjLabel = Label(self.framePrjSelect, text='Select your project:')
             prjLabel.pack(side=TOP, anchor=N)
             for index, prjName in enumerate(projects):
                 button = Button(framePrjButtons, text=prjName,
                                 command=partial(lambda i=index, prj=prjName: self.__onProjectSelect(i, prj)))
-                button.pack(side=LEFT)
+                button.pack(side=LEFT, padx=10)
                 self.btnsProjects.append(button)
-            print(self.btnsProjects)
         else:
             # TODO: Implement input
-            prjLabel = Label(framePrjSelect, text='Too many projects to list. Write the name here:')
+            prjLabel = Label(self.framePrjSelect, text='Too many projects to list. Write the name here:')
             # a1 = Entry(window).place(x=80, y=50)
 
-        frame2 = Frame(self.window, height=50, width=800, relief=RAISED, bg='white')
-        frame2.pack(fill=X)
-
-        self.window.mainloop()
-
-    def __basigGUIsetup(self):
-        self.window.title('DaVinci Resolve Automated Render')
-        self.window.geometry('800x500')
-
-    def __getProjectsList(self) -> [str]:
-        pm = self.resolve.GetProjectManager()
-        prjlist = pm.GetProjectListInCurrentFolder()
-        return prjlist
-
-    def __onProjectSelect(self, index: int, prjName: str):
+    def __onProjectSelect(self, index: int, prjName: str) -> None:
+        """
+        Action handler for project selection buttons
+        :param index: position of the button inside the btnsProjects list
+        :param prjName: string name of the project, as received from Resolve
+        """
         for btn in self.btnsProjects:
             btn['state'] = 'normal'
         self.btnsProjects[index]['state'] = 'disabled'
-        self.selectedProject = prjName
-        pass
+        self.selectedProject = self.pm.LoadProject(prjName)
+        self.__generateBinSelectionButtons()
+
+    def __generateBinSelectionButtons(self) -> None:
+        if self.selectedProject:
+            self.btnsFolders = []
+            self.mediaPool = self.selectedProject.GetMediaPool()
+            rootFolder = self.mediaPool.GetRootFolder()
+            subfolders = rootFolder.GetSubFolderList()
+
+            if self.frameFolderSelect.winfo_children():
+                for w in self.frameFolderSelect.winfo_children():
+                    w.destroy()
+
+            selectFolderLabel = Label(self.frameFolderSelect, text='Select the folder to be processed:')
+            selectFolderLabel.pack(side=TOP, anchor=N)
+
+            for index, folder in enumerate(subfolders):
+                folderName = folder.GetName()
+                button = Button(self.frameFolderSelect, text=folderName,
+                                command=partial(lambda i=index, prj=folderName: self.__onProjectSelect(i, prj)))
+                button.pack(side=LEFT, padx=10)
+                self.btnsFolders.append(button)
 
 
 if __name__ == '__main__':
