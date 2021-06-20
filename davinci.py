eca6e6da0dcee2e99da5687ce8d0caccb8a5f000
Marius Stuparu <marius@mariusstuparu.com>
2021-06-20 15:14:01 +0300

    split davinci and tk into two classes

---
+++ davinci.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+from sys import platform
+import importlib
+
+
+class DaVinciResolve:
+    def __init__(self, ):
+        self.errorMessages = []
+
+        self.resolve = None
+        self.pm = None
+        self.mediaStorage = None
+        self.selectedProject = None
+        self.mediaPool = None
+        self.rootFolder = None
+        self.selectedFolder = None
+
+        try:
+            import DaVinciResolveScript as dvr
+        except ImportError:
+            expectedPath = "/Library/Application Support/Blackmagic Design/DaVinci Resolve/Developer/Scripting/Modules/"
+            if platform.startswith("darwin"):
+                expectedPath = "/Library/Application Support/Blackmagic Design/DaVinci Resolve/Developer/Scripting/Modules/"
+            elif platform.startswith("win") or platform.startswith("cygwin"):
+                import os
+                expectedPath = os.getenv(
+                    'PROGRAMDATA') + "\\Blackmagic Design\\DaVinci Resolve\\Support\\Developer\\Scripting\\Modules\\"
+            elif platform.startswith("linux"):
+                expectedPath = "/opt/resolve/libs/Fusion/Modules/"
+            try:
+                self.dvr = importlib.import_module('DaVinciResolveScript', expectedPath + 'DaVinciResolveScript.py')
+            except ImportError:
+                self.errorMessages.append({
+                    'type': 'missing required library',
+                    'message': f'DaVinci Resolve library not found in {expectedPath}'
+                })
+        except AttributeError:
+            self.errorMessages.append({
+                'type': 'davinci_not_started',
+                'message': 'DaVinci Resolve is not running. Make sure you start Resolve before running this app.'
+            })
+        finally:
+            if dvr:
+                self.resolve = dvr.scriptapp('Resolve')
+            elif self.dvr:
+                self.resolve = self.dvr.scriptapp('Resolve')
+            self.pm = self.resolve.GetProjectManager()
+            self.mediaStorage = self.resolve.GetMediaStorage()
+
+    def getProjects(self):
+        if self.pm:
+            return self.pm.GetProjectListInCurrentFolder()
+        else:
+            return None
+
+    def loadProject(self, projectName: str):
+        if self.pm:
+            self.selectedProject = self.pm.LoadProject(projectName)
+            return self.selectedProject
+        else:
+            return None
+
+    def getRootFolders(self):
+        if self.selectedProject:
+            self.mediaPool = self.selectedProject.GetMediaPool()
+            self.rootFolder = self.mediaPool.GetRootFolder()
+            return self.rootFolder.GetSubFolderList()
+        else:
+            return None
+
+    def setCurrentFolder(self, folder):
+        if self.mediaPool:
+            self.mediaPool.SetCurrentFolder(folder)
+            return folder
+        else:
+            return None
+
+    def getFolderContent(self):
+        audioClips = []
+        videoClips = []
+        timelines = []
+        clipsInFolder = self.selectedFolder.GetClips()
+
+        for index in clipsInFolder:
+            clip = clipsInFolder[index]
+            if clip.GetClipProperty('Type') == 'Audio':
+                audioClips.append(clip)
+            if clip.GetClipProperty('Type') == 'Video':
+                videoClips.append(clip)
+            if clip.GetClipProperty('Type') == 'Timeline':
+                timelines.append(clip)
+
+        return {
+            'audioClips': audioClips,
+            'videoClips': videoClips,
+            'timelines': timelines
+        }
+
+
+if __name__ == '__main__':
+    pass
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 from os import environ as env
-from sys import platform
-import importlib
 from tkinter import *
 from functools import partial
-import time
+
+from davinci import DaVinciResolve
 
 
 class ResolveAutomation:
@@ -12,52 +11,10 @@ class ResolveAutomation:
         """
         Initialize variables and load DaVinci scripting support
         """
-        global dvr
-        self.errorMessages = []
         self.window = Tk()
-        # Resolve specific
-        self.resolve = None
-        self.pm = None
-        self.mediaStorage = None
-        self.selectedProject = None
-        self.mediaPool = None
-        self.rootFolder = None
-        self.firstLevelFolders = None
-        self.selectedFolder = None
-        self.clipsInFolder = None
+        self.resolve = DaVinciResolve()
 
-        try:
-            import DaVinciResolveScript as dvr
-        except ImportError:
-            expectedPath = "/Library/Application Support/Blackmagic Design/DaVinci Resolve/Developer/Scripting/Modules/"
-            if platform.startswith("darwin"):
-                expectedPath = "/Library/Application Support/Blackmagic Design/DaVinci Resolve/Developer/Scripting/Modules/"
-            elif platform.startswith("win") or platform.startswith("cygwin"):
-                import os
-                expectedPath = os.getenv(
-                    'PROGRAMDATA') + "\\Blackmagic Design\\DaVinci Resolve\\Support\\Developer\\Scripting\\Modules\\"
-            elif platform.startswith("linux"):
-                expectedPath = "/opt/resolve/libs/Fusion/Modules/"
-            try:
-                self.dvr = importlib.import_module('DaVinciResolveScript', expectedPath + 'DaVinciResolveScript.py')
-            except ImportError:
-                self.errorMessages.append({
-                    'type': 'missing required library',
-                    'message': f'DaVinci Resolve library not found in {expectedPath}'
-                })
-        except AttributeError:
-            self.errorMessages.append({
-                'type': 'davinci_not_started',
-                'message': 'DaVinci Resolve is not running. Make sure you start Resolve before running this app.'
-            })
-        finally:
-            if dvr:
-                self.resolve = dvr.scriptapp('Resolve')
-            elif self.dvr:
-                self.resolve = self.dvr.scriptapp('Resolve')
-            self.pm = self.resolve.GetProjectManager()
-            self.mediaStorage = self.resolve.GetMediaStorage()
-            self.__startGUI()
+        self.__startGUI()
 
     def __startGUI(self) -> None:
         """
@@ -88,13 +45,15 @@ class ResolveAutomation:
         """
         self.btnsProjects = []
 
-        projects = self.pm.GetProjectListInCurrentFolder()
+        projects = self.resolve.getProjects()
 
         framePrjButtons = Frame(self.framePrjSelect, height=50, width=800)
         framePrjButtons.pack(side=BOTTOM, pady=5)
+
         if len(projects) <= 9:
             prjLabel = Label(self.framePrjSelect, text='Select your project:')
             prjLabel.pack(side=TOP, anchor=N)
+
             for index, prjName in enumerate(projects):
                 button = Button(framePrjButtons, text=prjName,
                                 command=partial(lambda i=index, prj=prjName: self.__onProjectSelect(i, prj)))
@@ -114,19 +73,16 @@ class ResolveAutomation:
         for btn in self.btnsProjects:
             btn['state'] = 'normal'
         self.btnsProjects[index]['state'] = 'disabled'
-        self.selectedProject = self.pm.LoadProject(prjName)
         self.__cleanupWindowOnProjectChange()
-        self.__generateBinSelectionButtons()
+        self.__generateBinSelectionButtons(self.resolve.loadProject(prjName))
 
-    def __generateBinSelectionButtons(self) -> None:
+    def __generateBinSelectionButtons(self, projectSelected) -> None:
         """
         Read folders from project root and generate selection buttons
         """
-        if self.selectedProject:
+        if projectSelected:
             self.btnsFolders = []
-            self.mediaPool = self.selectedProject.GetMediaPool()
-            self.rootFolder = self.mediaPool.GetRootFolder()
-            self.firstLevelFolders = self.rootFolder.GetSubFolderList()
+            self.firstLevelFolders = self.resolve.getRootFolders()
 
             if len(self.firstLevelFolders):
                 selectFolderLabel = Label(self.frameFolderSelect, text='Select the folder to be processed:')
@@ -148,8 +104,7 @@ class ResolveAutomation:
         for btn in self.btnsFolders:
             btn['state'] = 'normal'
         self.btnsFolders[index]['state'] = 'disabled'
-        self.mediaPool.SetCurrentFolder(folder)
-        self.selectedFolder = self.mediaPool.GetCurrentFolder()
+        self.selectedFolder = self.resolve.setCurrentFolder(folder)
         self.__cleanupWindowOnFolderChange()
         self.__getFolderContents()
 
@@ -157,67 +112,57 @@ class ResolveAutomation:
         """
         Read folder contents and separate media into audio, video and timelines
         """
-        self.clipsInFolder = self.selectedFolder.GetClips()
-        self.audioFilesInFolder = []
-        self.videoFilesInFolder = []
-        self.timelinesInFolder = []
-
-        for index in self.clipsInFolder:
-            clip = self.clipsInFolder[index]
-            if clip.GetClipProperty('Type') == 'Audio':
-                self.audioFilesInFolder.append(clip)
-            if clip.GetClipProperty('Type') == 'Video':
-                self.videoFilesInFolder.append(clip)
-            if clip.GetClipProperty('Type') == 'Timeline':
-                self.timelinesInFolder.append(clip)
+        self.clipsInFolder = self.resolve.getFolderContent()
 
         stats = Label(self.frameClipsInfo,
                       text=f'Folder contains: '
-                           f'{len(self.audioFilesInFolder)} audio file(s), '
-                           f'{len(self.videoFilesInFolder)} video file(s) and '
-                           f'{len(self.timelinesInFolder)} timeline(s)')
+                           f'{len(self.clipsInFolder["audioClips"])} audio file(s), '
+                           f'{len(self.clipsInFolder["videoClips"])} video file(s) and '
+                           f'{len(self.clipsInFolder["timelines"])} timeline(s)')
         stats.pack(side=TOP, anchor=N)
-        if len(self.videoFilesInFolder) == 1 and len(self.audioFilesInFolder) >= 1:
+        if len(self.clipsInFolder["videoClips"]) == 1 and len(self.clipsInFolder["audioClips"]) >= 1:
             self.__showProcessButton()
 
     def __showProcessButton(self) -> None:
-        self.processingEnabled = True
-        self.buttonProcess = Button(self.frameProcessFolder, text='START', command=self.__processFolder)
+        self.buttonProcess = Button(self.frameProcessFolder, text='START', command=self.__startProcessing)
         self.buttonProcess.pack(padx=5, pady=15, side=RIGHT)
         self.buttonStop = Button(self.frameProcessFolder, text='Cancel', command=self.__cancelProcessing)
         self.buttonStop.pack(padx=5, pady=15, side=RIGHT)
         self.buttonStop['state'] = 'disabled'
         pass
 
-    def __processFolder(self):
+    def __startProcessing(self):
         self.buttonProcess['state'] = 'disabled'
         self.buttonStop['state'] = 'normal'
-        # for track in self.audioFilesInFolder:
-        #     self.buttonProcess['state'] = 'disabled'
-        #     self.buttonStop['state'] = 'normal'
-        #     print(f'processing... {track}')
-        #     time.sleep(5)
-        # else:
-        #     self.buttonProcess['state'] = 'normal'
-        #     self.buttonStop['state'] = 'disabled'
+        self.window.poll = True
+        self.__processFolder()
 
     def __cancelProcessing(self):
-        self.processingEnabled = False
+        self.window.poll = False
+        self.buttonProcess['state'] = 'normal'
+        self.buttonStop['state'] = 'disabled'
+
+    def __processFolder(self):
+        if self.window.poll:
+            if len(self.clipsInFolder["audioClips"]):
+                pass
+            # print(f'processing...')
+            # self.window.after(1000, self.__processFolder)
 
     def __cleanupWindowOnProjectChange(self):
+        """
+        Remove all generated buttons on project change
+        """
         if self.frameFolderSelect.winfo_children():
             for w in self.frameFolderSelect.winfo_children():
                 w.destroy()
 
-        if self.frameClipsInfo.winfo_children():
-            for w in self.frameClipsInfo.winfo_children():
-                w.destroy()
-
-        if self.frameProcessFolder.winfo_children():
-            for w in self.frameProcessFolder.winfo_children():
-                w.destroy()
+        self.__cleanupWindowOnFolderChange()
 
     def __cleanupWindowOnFolderChange(self):
+        """
+        Remove process-specific buttons on folder change
+        """
         if self.frameClipsInfo.winfo_children():
             for w in self.frameClipsInfo.winfo_children():
                 w.destroy()
